53ec3f6f2dc7ceac160441ca6c22581a5e34c64b
yonghyen <ankim2son@gmail.com>
2021-05-12 19:15:41 +0900

    promotion swiper 중간 저장

--- js/main.js
+++ js/main.js
@@ -44,9 +44,26 @@ fadeEls.forEach(function(fadeEl, index){
   });
 });
 
-const swiper = new Swiper('.notice-line .swiper-container', {
+new Swiper('.notice-line .swiper-container', {
   // Optional parameters
   direction: 'vertical',
   autoplay: true,
   loop: true
+});
+new Swiper('.promotion .swiper-container',{
+  slidesPerView : 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides:true,//1번 슬라이드가 가운데 보이기
+  loop:true,
+  autoplay: {
+    delay : 5000
+  },
+  pagination: {
+    el:'.promotion .swiper-pagination',//페이지 번호 요소 선택자
+    clickable : true
+  },
+  navigation: {
+    prevEl:'.promotion .swiper-prev',
+    nextEl:'.promotion .swiper-next'
+  }
 });
